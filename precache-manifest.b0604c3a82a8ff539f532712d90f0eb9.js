1091b35707b8e598e97cdbc6934b5802a5e146c4
Nitesh Shrestha <nitesh.shresthax@gmail.com>
2019-07-25 11:02:45 +0545

    Updates

---
+++ precache-manifest.b0604c3a82a8ff539f532712d90f0eb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5d28ca3f5deca59d114a5eeb9830fd",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "a5a0c9847eda470e21ec",
+    "url": "/hacker-news/static/css/main.9887b995.chunk.css"
+  },
+  {
+    "revision": "6ee6d5202dd77a0c542b",
+    "url": "/hacker-news/static/js/2.d3bf05f9.chunk.js"
+  },
+  {
+    "revision": "a5a0c9847eda470e21ec",
+    "url": "/hacker-news/static/js/main.4febb6a9.chunk.js"
+  },
+  {
+    "revision": "f4cbfa7cef56548df7d2",
+    "url": "/hacker-news/static/js/runtime~main.393a1947.js"
+  }
+]);
